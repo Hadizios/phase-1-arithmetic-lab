9168b9773d2672afcd51d37048462a52ade9150a
Hadizios <hadi.maleh@gmail.com>
2023-02-16 00:49:29 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+Math.floor(Math.random() * 10);
+let random = 10;
+
+let num3 = 44;
+let num4 = 10;
+const mod = num3 % num4;
+
+const max = (1, 2, 3, 4, 20);
